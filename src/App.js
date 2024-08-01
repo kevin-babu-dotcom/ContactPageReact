af0a22e1a7f357ee6acf22dababeb36db8f0f3dc
Kevin Babu <kevinbabu9@gmail.com>
2024-08-01 12:33:28 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+  import "./App.css";
+  import { Card, CardContent, Grid, TextField, Button, Typography } from "@mui/material";
+  import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  function App() {
 
-export default App;
+    const submitHandler = (e) => {
+      e.preventDefault()
+    }
+
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" ,  background: "linear-gradient(135deg, #f5f7fa, #c3cfe2)"}}>
+
+      
+      <Card style={{maxWidth:450, margin:"0 auto", padding:"20px 5px",borderRadius: 16, boxShadow: '0 4px 8px rgba(0,0,0,0.2)'}}>
+        <CardContent>
+          <Typography gutterBottom variant="h6" align="center" style={{  marginBottom: '1rem'}}>CONTACT US</Typography>
+          <form>
+          <Grid container spacing={1}>  
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="First Name"
+                placeholder="Enter First Name"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="Last Name"
+                placeholder="Enter Last Name"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                type="email"
+                label="Email"
+                placeholder="Enter your Email-ID"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                type="number"
+                label="Phone"
+                placeholder="Enter your Phone.No"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                label="Message"
+                multiline
+                rows={4}
+                placeholder="Enter your Message"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12}>
+              <Button type="submit" variant="contained" color="primary" fullWidth style={{ marginTop: 16 }} onClick={submitHandler}>
+                Submit
+                {/* e.preventDefault() */}
+              </Button>
+            </Grid>
+          </Grid>
+          </form>
+        </CardContent>
+      </Card>
+      </div>
+    );
+  }
+
+  export default App;
